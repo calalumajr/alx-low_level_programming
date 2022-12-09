49ef7a6efc82b120f1b9522c0e13234abe307f27
calalumajr <calalumajr@gmail.com>
2022-12-09 08:05:15 -0800

    101.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #!/bin/bash
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, str1, 59);
-	return (1);
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str, 59);
+return (1);
 }
