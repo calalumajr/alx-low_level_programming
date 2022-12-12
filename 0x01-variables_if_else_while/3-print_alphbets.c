18a4396f96305c233cd6f03bb010a8016fe39432
calalumajr <calalumajr@gmail.com>
2022-12-12 07:04:40 -0800

    3.

--- 0x01-variables_if_else_while/3-print_alphbets.c
+++ 0x01-variables_if_else_while/3-print_alphbets.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char alpha;
+	char alpha;
+	char bet;
 
-for (alpha = 'a'; alpha <= 'z'; alpha++)
-putchar(alpha);
-putchar('\n');
-return (0);
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
+		putchar(alpha);
+	}
+	for (bet = 'A' ; bet <= 'Z' ; bet++)
+	{
+		putchar(bet);
+	}
+	putchar('\n');
+	return (0);
 }
