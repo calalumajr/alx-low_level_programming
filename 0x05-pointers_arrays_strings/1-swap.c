2336d08a50ecedab0fa9020032007a8b1da6854e
calalumajr <calalumajr@gmail.com>
2022-12-20 14:09:14 -0800

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.c"
+/**
+* swap_int - check the code for Main.
+* @a: variable.
+* @b: variable.
+*/
+
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *b;
+*b = *a;
+*a = temp;
+}
