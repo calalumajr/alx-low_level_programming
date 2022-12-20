25172d2f9e9cfe0b8767d8d41b7620a92ff5f899
calalumajr <calalumajr@gmail.com>
2022-12-20 14:20:47 -0800

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include <string.h>
+
+/**
+ * _puts - check the code for Main.
+ * @str: variable.
+ */
+void _puts(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
