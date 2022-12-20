1dd2e4540560308e2a92d6893a10b36bf45caa5a
calalumajr <calalumajr@gmail.com>
2022-12-20 14:35:42 -0800

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code for Holberton School students.
+ * @s: variable.
+ */
+void print_rev(char *s)
+{
+int i;
+int a;
+for (i = 0; s[i] != '\0'; i++)
+{
+continue;
+}
+i--;
+for (a = 0; a <= i; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
