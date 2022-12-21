2dbfdcfba3cad4aa74b77da113b4b3b3380db934
calalumajr <calalumajr@gmail.com>
2022-12-21 01:17:29 -0800

    100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+
+/**
+ *
+ * * print_times_table - Entry point
+ *
+ * *If n is greater than 15 or less than 0 the function should not print anything
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
+void print_times_table(int n)
+
+{
+
+	int a;
+
+	a = 15;
+
+	{
+
+		for (a = 0; a <= 15;)
+
+		{
+
+			 _putchar(2);
+
+			 _putchar('\n');
+
+			 _putchar('25');
+
+			 _putchar('\n');
+
+		}
+
+	} 
+
+	return (0);
+
+}
