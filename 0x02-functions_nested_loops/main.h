c527865dd304cb23c78b32125b8a1979acd229d2
calalumajr <calalumajr@gmail.com>
2022-12-21 00:28:32 -0800

    4

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,29 +1,39 @@
-int _putchar(char c);
+#ifndef main_h
 
-void print_alphabet(void);
+	#define main_h
 
-void print_alphabet_x10(void);
 
-int _islower(int c);
 
-int _isalpha(int c);
 
-int _abs(int);
 
-int print_last_digit(int nld);
+	int _putchar(char);
 
-int print_last_digit(int nld);
+		void print_alphabet(void);
 
-void jack_bauer(void);
+			void print_alphabet_x10(void);
 
-void times_table(void);
+				int _islower(int c);
 
-int add(int one, int two);
+					int _isalpha(int c);
 
-void print_to_98(int n);
+						int print_sign(int n);
 
-void print_alphabet_x10(void);
+							int _abs(int);
 
-void print_times_table(int n);
+								int print_last_digit(int);
 
-int print_sign(int n);
+									void jack_bauer(void);
+
+										void times_table(void);
+
+											int add(int, int);
+
+												void print_to_98(int n);
+
+													void print_times_table(int n);
+
+														
+
+
+
+														#endif
