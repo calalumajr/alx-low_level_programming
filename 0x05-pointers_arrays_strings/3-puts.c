998b8c5eeb5ffdb498a2b67ad2fac523d4da659c
calalumajr <calalumajr@gmail.com>
2022-12-21 14:04:32 -0800

    3hf

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,25 +1,13 @@
 #include "main.h"
-
 /**
- *
- *   * _puts - prints a string, followed by a new line, to stdout
- *
- *     * @str: string to print
- *
- *       */
-
+  * _puts - prints a string, followed by a new line, to stdout
+  * @str: string to print
+  */
 void _puts(char *str)
-
 {
-
-		while (*str != '\0')
-
-				{
-
-							_putchar(*str++);
-
-								}
-
-			_putchar('\n');
-
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,47 +1,24 @@
 #include "main.h"
-
 /**
- *
- *  * print_rev - imprime en reversa
- *
- *   * @s: string
- *
- *    * return: 0
- *
- *     */
-
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
 void print_rev(char *s)
-
 {
+	int longi = 0;
+	int o;
 
-		int longi = 0;
-
-			int o;
-
-
-
-				while (*s != '\0')
-
-						{
-
-									longi++;
-
-											s++;
-
-												}
-
-					s--;
-
-						for (o = longi; o > 0; o--)
-
-								{
-
-											_putchar(*s);
-
-													s--;
-
-														}
-
-							_putchar('\n');
-
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
 }
