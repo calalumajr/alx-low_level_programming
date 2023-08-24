f980362f912ab974732405ada4d13e252bad10f3
calalumajr <calalumajr@gmail.com>
2023-08-23 19:39:13 -0700

    tmt3

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -4,10 +4,10 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-	printf("%d\n", _strlen("My Dyn Lib"));
-	return (0);
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
 }
